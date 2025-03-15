02581260669d8188c3d5a099060fdbb003832f66
watanabe0055 <mailok1212+github@gmail.com>
2025-03-15 17:08:00 +0900

    add: expo-image-pickerを追加し、画像選択機能を実装

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,18 +1,32 @@
 import { StyleSheet, View } from "react-native";
 import { registerRootComponent } from "expo";
+import * as ImagePicker from "expo-image-picker";
 
 import PlaceholderImage from "@/assets/images/background-image.png";
 import { Button } from "@/components/button";
 import { ImageViewer } from "@/components/image-viewer";
 
 const App = () => {
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("画像が選択されていません");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="写真を選択" />
+        <Button theme="primary" label="写真を選択" onPress={pickImageAsync} />
         <Button label="この写真を使用" />
       </View>
     </View>
